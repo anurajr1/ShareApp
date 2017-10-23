295f5880c63b6e5df7240c185afcc95e59d6ce51
Anuraj R <anuraj.r@sap.com>
2017-10-23 16:24:04 +0530

    apps ascending order sorting done

--- app/src/main/java/com/anu/developers3k/shareapp/SystemAppsFragment.java
+++ app/src/main/java/com/anu/developers3k/shareapp/SystemAppsFragment.java
@@ -16,8 +16,18 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 
+import com.anu.developers3k.shareapp.adapter.AppsManager;
 import com.anu.developers3k.shareapp.adapter.SystemAppAdapter;
 import com.anu.developers3k.shareapp.adapter.SystemAppsManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class SystemAppsFragment extends Fragment {
     RecyclerView recyclerView;
@@ -25,6 +35,7 @@ public class SystemAppsFragment extends Fragment {
     private Context mContext;
     private RelativeLayout mRelativeLayout;
     private RecyclerView.Adapter mAdapter;
+    private List<String> mDataSet =  new ArrayList<String>();
     View rootView;
     public SystemAppsFragment(){}
     @Override
@@ -40,9 +51,57 @@ public class SystemAppsFragment extends Fragment {
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
+        try {
+            final HashMap<String, String> map = new HashMap<String, String>();
+            if (!new SystemAppsManager(getContext()).getInstalledPackages().isEmpty()) {
+                for (String data : new SystemAppsManager(getContext()).getInstalledPackages()) {
+                    // Initialize a new instance of AppManager class
+                    AppsManager appsManager = new AppsManager(mContext);
+                    // Get the current app label
+                    String label = appsManager.getApplicationLabelByPackageName(data);
+                    map.put(data, label);
+                }
+            }
+
+            // get entrySet from HashMap object
+            Set<Map.Entry<String, String>> AppSet = map.entrySet();
+
+            // convert HashMap to List of Map entries
+            List<Map.Entry<String, String>> AppListEntry =
+                    new ArrayList<Map.Entry<String, String>>(AppSet);
+
+            // sort list of entries using Collections class utility method sort(ls, cmptr)
+            Collections.sort(AppListEntry,
+                    new Comparator<Map.Entry<String, String>>() {
+                        @Override
+                        public int compare(Map.Entry<String, String> es1,
+                                           Map.Entry<String, String> es2) {
+                            return es1.getValue().compareTo(es2.getValue());
+                        }
+                    });
+
+            // store into LinkedHashMap for maintaining insertion order
+            Map<String, String> AppLHMap =
+                    new LinkedHashMap<String, String>();
+
+            // iterating list and storing in LinkedHahsMap
+            for (Map.Entry<String, String> map1 : AppListEntry) {
+                AppLHMap.put(map1.getKey(), map1.getValue());
+            }
+
+            // iterate LinkedHashMap to retrieved stored values
+            for (Map.Entry<String, String> lhmap : AppLHMap.entrySet()) {
+                System.out.println("Key : " + lhmap.getKey() + "\t\t"
+                        + "Value : " + lhmap.getValue());
+                mDataSet.add(lhmap.getKey());
+            }
+        }catch (Exception e){
+            System.out.print("error caught");
+        }
+
         mAdapter = new SystemAppAdapter(
                 getContext(),
-                new SystemAppsManager(getContext()).getInstalledPackages()
+                mDataSet
         );
         recyclerView.setAdapter(mAdapter);
         return rootView;
--- app/src/main/java/com/anu/developers3k/shareapp/UserAppsFragment.java
+++ app/src/main/java/com/anu/developers3k/shareapp/UserAppsFragment.java
@@ -5,6 +5,7 @@ package com.anu.developers3k.shareapp;
  */
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,6 +18,15 @@ import android.widget.RelativeLayout;
 import com.anu.developers3k.shareapp.adapter.AppsManager;
 import com.anu.developers3k.shareapp.adapter.InstalledAppAdapter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class UserAppsFragment extends Fragment {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
@@ -24,6 +34,7 @@ public class UserAppsFragment extends Fragment {
     private RelativeLayout mRelativeLayout;
     private RecyclerView.Adapter mAdapter;
     View rootView;
+    private List<String> mDataSet =  new ArrayList<String>();
     public UserAppsFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,11 +48,58 @@ public class UserAppsFragment extends Fragment {
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
+       final HashMap<String, String> map = new HashMap<String, String>();
+        if(!new AppsManager(getContext()).getInstalledPackages().isEmpty()) {
+            for (String data : new AppsManager(getContext()).getInstalledPackages()) {
+                // Initialize a new instance of AppManager class
+                AppsManager appsManager = new AppsManager(mContext);
+                // Get the current app label
+                String label = appsManager.getApplicationLabelByPackageName(data);
+                map.put(data,label);
+            }
+        }
+
+        // get entrySet from HashMap object
+        Set<Map.Entry<String, String>> AppSet = map.entrySet();
+
+        // convert HashMap to List of Map entries
+        List<Map.Entry<String, String>> AppListEntry =
+                new ArrayList<Map.Entry<String, String>>(AppSet);
+
+        // sort list of entries using Collections class utility method sort(ls, cmptr)
+        Collections.sort(AppListEntry,
+                new Comparator<Map.Entry<String, String>>() {
+                    @Override
+                    public int compare(Map.Entry<String, String> es1,
+                                       Map.Entry<String, String> es2) {
+                        return es1.getValue().compareTo(es2.getValue());
+                    }
+                });
+
+        // store into LinkedHashMap for maintaining insertion order
+        Map<String, String> AppLHMap =
+                new LinkedHashMap<String, String>();
+
+        // iterating list and storing in LinkedHahsMap
+        for(Map.Entry<String, String> map1 : AppListEntry){
+            AppLHMap.put(map1.getKey(), map1.getValue());
+        }
+
+        // iterate LinkedHashMap to retrieved stored values
+        for(Map.Entry<String, String> lhmap : AppLHMap.entrySet()){
+            System.out.println("Key : "  + lhmap.getKey() + "\t\t"
+                    + "Value : "  + lhmap.getValue());
+            mDataSet.add(lhmap.getKey());
+        }
+
+
+
         mAdapter = new InstalledAppAdapter(
                 getContext(),
-                new AppsManager(getContext()).getInstalledPackages()
+                mDataSet
         );
         recyclerView.setAdapter(mAdapter);
+
         return rootView;
     }
 }
