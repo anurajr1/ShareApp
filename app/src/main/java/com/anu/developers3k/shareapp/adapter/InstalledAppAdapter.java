dcae20028a1cb0a8ac0f3c3712be95135020a095
Anuraj R <anuraj.r@sap.com>
2017-10-24 17:11:06 +0530

    slider added, need to fix

--- app/src/main/java/com/anu/developers3k/shareapp/adapter/InstalledAppAdapter.java
+++ app/src/main/java/com/anu/developers3k/shareapp/adapter/InstalledAppAdapter.java
@@ -1,26 +1,31 @@
 package com.anu.developers3k.shareapp.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.anu.developers3k.shareapp.R;
+import com.mancj.slideup.SlideUp;
+import com.mancj.slideup.SlideUpBuilder;
 
 import java.util.List;
+
+import static android.R.id.primary;
+import static java.security.AccessController.getContext;
 
 public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapter.ViewHolder> {
     private Context mContext;
     private List<String> mDataSet;
+    private SlideUp slideUp;
+    public View sliderView;
 
     public InstalledAppAdapter(Context context, List<String> list){
         mContext = context;
@@ -33,6 +38,8 @@ public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapte
             public ImageView mImageViewIcon;
             public ImageView mImageOpenInApp;
 
+
+
             public ViewHolder (View v){
                 super(v);
                 // Get the widgets reference from custom layout
@@ -41,6 +48,7 @@ public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapte
                 mTextViewPackage = (TextView) v.findViewById(R.id.app_package);
                 mImageViewIcon = (ImageView) v.findViewById(R.id.iv_icon);
                 mImageOpenInApp =(ImageView) v.findViewById(R.id.openinapp);
+
             }
     }
 
@@ -53,7 +61,7 @@ public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // Initialize a new instance of AppManager class
         AppsManager appsManager = new AppsManager(mContext);
 
@@ -84,12 +92,35 @@ public class InstalledAppAdapter extends RecyclerView.Adapter<InstalledAppAdapte
             @Override
             public void onClick(View view) {
                 // Get the intent to launch the specified application
-                Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(packageName);
-                if (intent != null) {
-                    mContext.startActivity(intent);
-                } else {
-                    Toast.makeText(mContext, packageName + " Launch Error.", Toast.LENGTH_SHORT).show();
-                }
+//                Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(packageName);
+//                if (intent != null) {
+//                    mContext.startActivity(intent);
+//                } else {
+//                    Toast.makeText(mContext, packageName + " Launch Error.", Toast.LENGTH_SHORT).show();
+//                }
+
+//                SlidrConfig config = new SlidrConfig.Builder()
+//                        .primaryColor(getResources().getColor(R.color.primary)
+//                                .secondaryColor(getResources().getColor(R.color.secondary)
+//                                        .position(SlidrPosition.LEFT)
+//                                        .sensitivity(1f)
+//                                        .scrimColor(Color.BLACK)
+//                                        .scrimStartAlpha(0.8f)
+//                                        .scrimEndAlpha(0f)
+//                                        .velocityThreshold(2400)
+//                                        .distanceThreshold(0.25f)
+//                                        .build();
+//                LayoutInflater  mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//                mInflater.inflate(R.layout.slideview, null, true);
+
+                View view1 = View.inflate(mContext, R.layout.slideview, null);
+                sliderView = view1.findViewById(R.id.slideView);
+//                slideUp = new SlideUp(slideView);
+//                slideUp.
+                slideUp = new SlideUpBuilder(sliderView)
+                        .withStartState(SlideUp.State.HIDDEN)
+                        .withStartGravity(Gravity.BOTTOM)
+                        .build();
             }
         });
 
