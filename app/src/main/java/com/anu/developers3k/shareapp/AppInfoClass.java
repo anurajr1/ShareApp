d789da2df96ca37995513820f013053ecccf2275
Anuraj R <anuraj.r@sap.com>
2017-11-10 17:37:38 +0530

    animation added with details fetching

--- app/src/main/java/com/anu/developers3k/shareapp/AppInfoClass.java
+++ app/src/main/java/com/anu/developers3k/shareapp/AppInfoClass.java
@@ -1,23 +1,41 @@
 package com.anu.developers3k.shareapp;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.anu.developers3k.shareapp.adapter.AppsManager;
+import com.transitionseverywhere.Fade;
+import com.transitionseverywhere.TransitionManager;
+import com.transitionseverywhere.TransitionSet;
+import com.transitionseverywhere.extra.Scale;
+
+import java.util.List;
+
 
 /**
  * Created by i321994 on 25/10/17.
  */
 
 public class AppInfoClass extends AppCompatActivity {
+    PackageManager pm = null;
+    private static int SPLASH_TIME_OUT = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appdetaillayout);
+
+        final ViewGroup transitionsContainer = (ViewGroup) findViewById(R.id.transitions_container);
 
         Intent intent = getIntent();
         String packageName = intent.getStringExtra("packagename");
@@ -36,30 +54,57 @@ public class AppInfoClass extends AppCompatActivity {
 
 
         //first install time
-        TextView firstInstallTime = (TextView)findViewById(R.id.text_versionname1);
+        final TextView firstInstallTime = (TextView)findViewById(R.id.text_versionname1);
         firstInstallTime.setText((appsManager.installTimeFromPackageManager(packageName).toString()));
 
         //update date time
-        TextView updateTime = (TextView)findViewById(R.id.text_versioncode1);
+        final TextView updateTime = (TextView)findViewById(R.id.text_versioncode1);
         updateTime.setText((appsManager.lastUpdateTimeFromPackageManager(packageName)).toString());
 
         String versionName = "";
         int versionCode = 0;
         try {
-            final PackageManager pm = getPackageManager();
+            pm = getPackageManager();
             versionName = pm.getPackageInfo(packageName, 0).versionName;
             versionCode = pm.getPackageInfo(packageName, 0).versionCode;
         }catch(Exception e){
             System.out.print("inside the error block");
         }
         //version name
-        TextView versionNametextView = (TextView)findViewById(R.id.text_versionname);
+        final TextView versionNametextView = (TextView)findViewById(R.id.text_versionname);
         versionNametextView.setText(versionName);
 
         //version code
-        TextView versionCodeTextView = (TextView)findViewById(R.id.text_versioncode);
+        final TextView versionCodeTextView = (TextView)findViewById(R.id.text_versioncode);
         versionCodeTextView.setText(Integer.toString(versionCode));
 
+
+        //animation initilization
+        TransitionManager.beginDelayedTransition(transitionsContainer);
+
+        //layout id
+        final LinearLayout layoutDetail = (LinearLayout)findViewById(R.id.linearid);
+        layoutDetail.setVisibility(View.GONE);
+
+
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                boolean visible=true;
+                TransitionSet set = new TransitionSet()
+                        .addTransition(new Scale(0.7f))
+                        .addTransition(new Fade())
+                        .setInterpolator(visible ? new LinearOutSlowInInterpolator() :
+                                new FastOutLinearInInterpolator());
+                TransitionManager.beginDelayedTransition(transitionsContainer, set);
+                layoutDetail.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+            }
+        }, SPLASH_TIME_OUT);
 
     }
 }
